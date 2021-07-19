22f70a324049214e1a21972668a5483e62f85c62
yesseniarod <yessi.rod30@gmail.com>
2021-07-19 20:17:23 +0000

    selected class, song, and video. #2

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var $timeSelect = document.querySelectorAll('.time-select button');
 var $buttonTime = document.querySelectorAll('button[data-time]');
 var $timeDisplay = document.querySelector('.time-display');
+var $play = document.querySelector('.play');
+var $song = document.querySelector('.song');
+var $video = document.querySelector('.video-container');
 
 
 $timeSelect.forEach(option => {
